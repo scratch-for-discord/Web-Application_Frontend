6d6e7ea5e2b641beb123eaebda28b61947e146a7
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-11-26 09:50:01 -0800

    beeble

--- src/toolbox.ts
+++ src/toolbox.ts
@@ -1,46 +1,136 @@
-let toolboxString = `<xml>
-        <category name="Logic" colour="%{BKY_LOGIC_HUE}">
-            <block type="controls_if"></block>
-            <block type="logic_compare"></block>
-            <block type="logic_operation"></block>
-            <block type="logic_negate"></block>
-            <block type="logic_boolean"></block>
-        </category>
-        <category name="Loops" colour="%{BKY_LOOPS_HUE}">
-            <block type="controls_repeat_ext">
-            <value name="TIMES">
-                <block type="math_number">
-                <field name="NUM">10</field>
-                </block>
-            </value>
-            </block>
-            <block type="controls_whileUntil"></block>
-        </category>
-        <category name="Math" colour="%{BKY_MATH_HUE}">
-            <block type="math_number">
-            <field name="NUM">123</field>
-            </block>
-            <block type="math_arithmetic"></block>
-            <block type="math_single"></block>
-        </category>
-        <category name="Text" colour="%{BKY_TEXTS_HUE}">
-            <block type="text"></block>
-            <block type="text_length"></block>
-            <block type="text_print"></block>
-        </category>
-        <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
-            </category>
-
-        <sep css-container="sepDark"></sep>
-        <category name='Base' colour='#c75bc2'>
-
-        <block type='on_bot_code_start'/>
-        <block type='bot_amount_info'/>
-        <block type="bot_shutdown_stop"/>
-        <block type="set_bot_game_stream"/>
-        <block type="env"/>
-        </category>
-
-        </xml>`
+// done in a seconds with https://replit.com/@RedMan13/blockly-xml-to-json-toolbox 
+let toolboxString = {
+    "kind": "flyoutToolbox",
+    "contents": [
+        {
+            "kind": "category",
+            "name": "Logic",
+            "colour": "%{BKY_LOGIC_HUE}",
+            "contents": [
+                {
+                    "type": "controls_if",
+                    "kind": "block"
+                },
+                {
+                    "type": "logic_compare",
+                    "kind": "block"
+                },
+                {
+                    "type": "logic_operation",
+                    "kind": "block"
+                },
+                {
+                    "type": "logic_negate",
+                    "kind": "block"
+                },
+                {
+                    "type": "logic_boolean",
+                    "kind": "block"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Loops",
+            "colour": "%{BKY_LOOPS_HUE}",
+            "contents": [
+                {
+                    "type": "controls_repeat_ext",
+                    "kind": "block",
+                    "inputs": {
+                        "TIMES": {
+                            "block": {
+                                "type": "math_number",
+                                "fields": {
+                                    "NUM": "10"
+                                }
+                            }
+                        }
+                    }
+                },
+                {
+                    "type": "controls_whileUntil",
+                    "kind": "block"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Math",
+            "colour": "%{BKY_MATH_HUE}",
+            "contents": [
+                {
+                    "type": "math_number",
+                    "kind": "block",
+                    "fields": {
+                        "NUM": "123"
+                    }
+                },
+                {
+                    "type": "math_arithmetic",
+                    "kind": "block"
+                },
+                {
+                    "type": "math_single",
+                    "kind": "block"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Text",
+            "colour": "%{BKY_TEXTS_HUE}",
+            "contents": [
+                {
+                    "type": "text",
+                    "kind": "block"
+                },
+                {
+                    "type": "text_length",
+                    "kind": "block"
+                },
+                {
+                    "type": "text_print",
+                    "kind": "block"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Variables",
+            "colour": "%{BKY_VARIABLES_HUE}"
+        },
+        {
+            "kind": "sep"
+        },
+        {
+            "kind": "category",
+            "name": "Base",
+            "colour": "#c75bc2",
+            "contents": [
+                {
+                    "type": "on_bot_code_start",
+                    "kind": "block"
+                },
+                {
+                    "type": "bot_amount_info",
+                    "kind": "block"
+                },
+                {
+                    "type": "bot_shutdown_stop",
+                    "kind": "block"
+                },
+                {
+                    "type": "set_bot_game_stream",
+                    "kind": "block"
+                },
+                {
+                    "type": "env",
+                    "kind": "block"
+                }
+            ]
+        }
+    ]
+}
 
 export { toolboxString }
