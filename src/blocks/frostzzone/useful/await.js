e1f9ced1dab395228c72fae2720056377962d0bb
frostzzone <65735427+frostzzone@users.noreply.github.com>
2023-02-23 06:54:08 -0800

    await block crazy

---
+++ src/blocks/frostzzone/useful/await.js
@@ -0,0 +1,22 @@
+Blockly.Blocks["frost_await"] = {
+  init: function () {
+    this.jsonInit({
+      "message0": "await %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "path"
+        }
+      ],
+      "output": null,
+      "inputsInline": true,
+      "colour": '#D14081',
+      "tooltip": "awaits code from block, search exclusive :O",
+      "helpUrl": ""
+    });
+  }
+};
+
+Blockly.JavaScript["frost_await"] = function (block) {
+  return [Blockly.JavaScript.valueToCode(block, "path", Blockly.JavaScript.ORDER_ATOMIC), Blockly.JavaScript.ORDER_ATOMIC];
+}
