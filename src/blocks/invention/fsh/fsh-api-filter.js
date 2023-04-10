bc44f7177303a4ae37e4e21e61e7d306246dde7c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-04-10 13:22:46 +0200

    Update fsh-api-filter.js

--- src/blocks/invention/fsh/fsh-api-filter.js
+++ src/blocks/invention/fsh/fsh-api-filter.js
@@ -1,1 +1,36 @@
+// temporary
 
+import Blockly from "blockly/core";
+
+const blockName = "inv_fsh_api_filter";
+
+const blockData = {
+  "message0": "Get HTML from %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "url",
+      "align": "CENTRE"
+    }
+  ],
+  "inputsInline": true,
+  "output": "String",
+  "colour": "#1a75ba",
+  "tooltip": "Gets the html code of a website (e.g. https://google.com)",
+  "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+  var value_url = Blockly.JavaScript.valueToCode(block, 'url', Blockly.JavaScript.ORDER_ATOMIC);
+  value_url = encodeURIComponent(value_url.toLowerCase());
+
+  var code = `await _S4D_inventionFSHapiHtml('${value_url}')`;
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
