1a446ea35bcec09cda1d73fe9a173d44bdce71a1
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-04-10 13:06:10 +0200

    Update index.js

--- src/blocks/invention/fsh/index.js
+++ src/blocks/invention/fsh/index.js
@@ -1,2 +1,7 @@
-import "./fsh-api-animal.js"
+// fsh api blocks
+// text api
 import "./fsh-api-html.js"
+import "./fsh-api-filter.js"
+import "./fsh-api-censor.js"
+// image api
+import "./fsh-api-animal.js"
