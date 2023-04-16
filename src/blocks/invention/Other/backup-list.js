42d47414481c0ee69868a8d8f1995771f276ed29
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-04-16 21:26:43 +0200

    Create backup-list.js

---
+++ src/blocks/invention/Other/backup-list.js
@@ -0,0 +1,25 @@
+import Blockly from "blockly/core";
+
+const blockName = "inv_backup_list";
+
+const blockData = {
+  "message0": "backups",
+  "output": "Array",
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "#187795",
+  "tooltip": "Gets list of backup ids",
+  "helpUrl": ""
+};
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+  var code = `_S4D_backups`;
+
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
