4c24f8b5558cd667820c2872b0d221cafec0f6d8
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-07 18:07:32 +0200

    Create comment-code.js

---
+++ src/blocks/invention/Other/comment-code.js
@@ -0,0 +1,37 @@
+import Blockly from "blockly/core";
+
+const blockName = "inv_comment_code";
+
+const blockData = {
+  "message0": "aaa %1 %2 %3",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "NAME",
+      "text": "default"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "NAME"
+    }
+  ],
+  "colour": 60,
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript['inv_chart_to_url'] = function() {
+  // this is giving me pain
+  var code = `chart.toURL()`;
+  
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
