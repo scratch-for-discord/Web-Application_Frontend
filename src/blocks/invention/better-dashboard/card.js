389bf8d3cd957b4019a0acaa06c962b4d64e09bb
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-19 21:33:32 +0200

    Create card.js

---
+++ src/blocks/invention/better-dashboard/card.js
@@ -0,0 +1,61 @@
+import Blockly from "blockly/core";
+
+const blockName = "inv/lars_soft_ui_card";
+
+const blockData = {
+  "message0": "Set Card to %1 Category %2 Title %3 Description %4 Image (src) %5 Link %6 %7",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "category"
+    },
+    {
+      "type": "input_value",
+      "name": "title"
+    },
+    {
+      "type": "input_value",
+      "name": "description"
+    },
+    {
+      "type": "input_value",
+      "name": "image"
+    },
+    {
+      "type": "field_checkbox",
+      "name": "check",
+      "checked": true
+    },
+    {
+      "type": "field_input",
+      "name": "link",
+      "text": "url"
+    }
+  ],
+  "inputsInline": false,
+  "colour": 210,
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+  /*var value_name = Blockly.JavaScript.valueToCode(block, 'name', Blockly.JavaScript.ORDER_ATOMIC);
+  var number_name = block.getFieldValue('NAME');
+  var value_token = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_client_id = Blockly.JavaScript.valueToCode(block, 'client id', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_client_secret = Blockly.JavaScript.valueToCode(block, 'client secret', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_email = Blockly.JavaScript.valueToCode(block, 'email', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_drop_color = block.getFieldValue('drop-color');*/
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
