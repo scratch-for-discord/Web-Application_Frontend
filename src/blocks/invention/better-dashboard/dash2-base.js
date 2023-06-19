f11245e583f843d720031e4c6b0f7c90d2299f5b
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-19 21:19:09 +0200

    Update dash2-base.js

--- src/blocks/invention/better-dashboard/dash2-base.js
+++ src/blocks/invention/better-dashboard/dash2-base.js
@@ -3,7 +3,7 @@ import Blockly from "blockly/core";
 const blockName = "inv/lars_soft_ui";
 
 const blockData = {
-  "message0": "Start SoftUI Dashboard with %1 Name %2 Port %3 %4 Token %5 License %6 Client ID %7 Client Secret %8 Support Email %9 Color Scheme %10 %11 Define Card %12 Inputs %13",
+  "message0": "Start SoftUI Dashboard with %1 Name %2 Port %3 %4 Token %5 License %6 Client ID %7 Client Secret %8 Support Email %9 Owner ID's %10 Color Scheme %11 %12 %13 %14 Define Card %15 Inputs %16",
   "args0": [
     {
       "type": "input_dummy"
@@ -15,7 +15,7 @@ const blockData = {
     {
       "type": "field_number",
       "name": "port",
-      "value": 0
+      "value": 8080
     },
     {
       "type": "input_dummy"
@@ -39,6 +39,10 @@ const blockData = {
     {
       "type": "input_value",
       "name": "email"
+    },
+    {
+      "type": "input_value",
+      "name": "owner"
     },
     {
       "type": "field_dropdown",
@@ -67,8 +71,22 @@ const blockData = {
         [
           "yellow",
           "yellow"
+        ],
+        [
+          "custom...",
+          "custom"
         ]
       ]
+    },
+    {
+      "type": "field_input",
+      "name": "primary",
+      "text": "primary hex"
+    },
+    {
+      "type": "field_input",
+      "name": "secondary",
+      "text": "secondary hex"
     },
     {
       "type": "input_dummy"
@@ -87,6 +105,7 @@ const blockData = {
   "tooltip": "",
   "helpUrl": ""
 }
+
 Blockly.Blocks[blockName] = {
     init: function() {
         this.jsonInit(blockData);
