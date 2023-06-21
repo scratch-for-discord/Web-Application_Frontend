6f4c08ab2533a460126e95f7b2330e57b5254a4d
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-21 17:17:50 +0200

    Update body.js

--- src/blocks/invention/html/body.js
+++ src/blocks/invention/html/body.js
@@ -3,16 +3,21 @@ import Blockly from "blockly/core";
 const blockName = "inv_html_body";
 
 const blockData = {
-  "message0": "body %1 %2",
+  "message0": "body %1 style %2 %3",
   "args0": [
     {
       "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "NAME"
     },
     {
       "type": "input_statement",
       "name": "NAME"
     }
   ],
+  "inputsInline": true,
   "previousStatement": null,
   "nextStatement": null,
   "colour": 180,
@@ -27,8 +32,9 @@ Blockly.Blocks[blockName] = {
 };
 
 Blockly.JavaScript[blockName] = function(block) {
+  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
   var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
-  var code = `"<body>
+  var code = `"<body${value_name != null ? "" : ' style="'+value_name+'"'}>
     ${statements_name}
   </body>"`;
   return code;
