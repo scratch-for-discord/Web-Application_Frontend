e8599f2afbc42f30d8297c824cd82437af016273
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-21 11:56:34 +0200

    Update html.js

--- src/blocks/invention/html/html.js
+++ src/blocks/invention/html/html.js
@@ -1,1 +1,31 @@
+import Blockly from "blockly/core";
 
+const blockName = "inv_html";
+
+const blockData = {
+  "message0": "html %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "NAME"
+    }
+  ],
+  "output": "String",
+  "colour": 180,
+  "tooltip": "Generates html",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block) {
+  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+  var code = `"<html>
+    ${statements_name}
+  </html>"`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
