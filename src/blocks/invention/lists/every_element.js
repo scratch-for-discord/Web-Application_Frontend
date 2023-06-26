5f17d888c8447be34b5724af9abcf6503f672e72
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-26 14:28:36 +0200

    Update every_element.js

--- src/blocks/invention/lists/every_element.js
+++ src/blocks/invention/lists/every_element.js
@@ -3,10 +3,13 @@ import Blockly from "blockly/core";
 const blockName = "inv_list_every_element";
 
 const blockData = {
-  "message0": "every element",
+  "message0": "current element on every",
   "args0": [],
   "inputsInline": true,
-  "output": "String",
+  "output": "[
+    "String",
+    "Number"
+  ],
   "colour": 270,
   "tooltip": "Element currently being selected (for the every list block)",
   "helpUrl": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every"
