86ec0b66a0167634c469ebd522af401214d88b34
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-26 14:08:52 +0200

    Create every_element.js

---
+++ src/blocks/invention/lists/every_element.js
@@ -0,0 +1,24 @@
+import Blockly from "blockly/core";
+
+const blockName = "inv_list_every_element";
+
+const blockData = {
+  "message0": "every element",
+  "args0": [],
+  "inputsInline": true,
+  "output": "String",
+  "colour": 270,
+  "tooltip": "Element currently being selected (for the every list block)",
+  "helpUrl": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every"
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+  var code = `element`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
