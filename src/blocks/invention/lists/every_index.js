6bde59e912b9650bd9938e06cb0ffbe47d66d762
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-06-26 14:28:08 +0200

    Update every_index.js

--- src/blocks/invention/lists/every_index.js
+++ src/blocks/invention/lists/every_index.js
@@ -3,10 +3,13 @@ import Blockly from "blockly/core";
 const blockName = "inv_list_every_index";
 
 const blockData = {
-  "message0": "every index",
+  "message0": "current index on every",
   "args0": [],
   "inputsInline": true,
-  "output": "String",
+  "output": [
+    "String",
+    "Number"
+  ],
   "colour": 270,
   "tooltip": "Index of the element currently being selected (for the every list block)",
   "helpUrl": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every"
