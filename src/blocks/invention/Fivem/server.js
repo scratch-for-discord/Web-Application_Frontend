784e98f4858c9f03d09797b1793a138ebe25b549
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-08-06 18:18:15 +0200

    Update server.js

--- src/blocks/invention/Fivem/server.js
+++ src/blocks/invention/Fivem/server.js
@@ -1,1 +1,29 @@
+import Blockly from "blockly/core";
 
+const blockName = "inv_fivem_server";
+
+const blockData = {
+  "message0": "Connect to fivem server %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "NAME",
+      "check": "String"
+    }
+  ],
+  "colour": "#CC8899",
+  "tooltip": "Connects to a fivem server",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function(block, generator) {
+  var value_name = generator.valueToCode(block, 'NAME', javascript.Order.ATOMIC);
+  var code = `const __S4D__fivem_server = new __S4D__fivem.DiscordFivemApi("${value_name}")`;
+  return code;
+};
