4decc01a445a337e670517bc53cde10df907f0cd
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-08-06 19:20:54 +0200

    Create port.js

---
+++ src/blocks/invention/Fivem/port.js
@@ -0,0 +1,22 @@
+import Blockly from "blockly/core";
+
+const blockName = "inv_fivem_port";
+
+const blockData = {
+  "message0": "port",
+  "output": "String",
+  "colour": "#CC8899",
+  "tooltip": "Port of server",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+  var code = `__S4D__fivem_server.port`;
+  return [code, Blockly.javascript.ORDER_NONE];
+};
