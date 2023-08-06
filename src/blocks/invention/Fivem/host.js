993a850a2a92b9af3abcd343cdaf3b0ac0f5bef1
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-08-06 19:20:17 +0200

    Create host.js

---
+++ src/blocks/invention/Fivem/host.js
@@ -0,0 +1,22 @@
+import Blockly from "blockly/core";
+
+const blockName = "inv_fivem_host";
+
+const blockData = {
+  "message0": "host",
+  "output": "String",
+  "colour": "#CC8899",
+  "tooltip": "Host of server",
+  "helpUrl": ""
+}
+
+Blockly.Blocks[blockName] = {
+    init: function() {
+        this.jsonInit(blockData);
+    }
+};
+
+Blockly.JavaScript[blockName] = function() {
+  var code = `__S4D__fivem_server.host`;
+  return [code, Blockly.javascript.ORDER_NONE];
+};
