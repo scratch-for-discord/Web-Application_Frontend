2dbb2c404ecd2630c4a2d393a2097c9ab94d15a1
Freslin <160232566+Freslin@users.noreply.github.com>
2024-02-26 19:01:47 +0530

    Update permissionsaddarray.js

--- src/blocks/Monaco/permissionsaddarray.js
+++ src/blocks/Monaco/permissionsaddarray.js
@@ -4,7 +4,7 @@ const blockName = "monaco_edit_permissions";
 
 
 const blockData = {
-    "message0": "in channel %1 %2 permissions to member/role %3 %4",
+    "message0": "in channel %1 %2 permissions to member/role with id %3 %4",
     "args0": [
        {
       "type": "input_value",
@@ -28,7 +28,7 @@ const blockData = {
     {
       "type": "input_value",
       "name": "member",
-      "check": "Role"
+      "check": "String"
     },
     {
       "type": "input_statement",
@@ -54,8 +54,12 @@ Blockly.JavaScript['monaco_edit_permissions'] = function(block) {
   var value_member = Blockly.JavaScript.valueToCode(block, 'member', Blockly.JavaScript.ORDER_ATOMIC);
   var statements = Blockly.JavaScript.statementToCode(block, 'then');
   // TODO: Assemble JavaScript into code variable.
-  var guilder = `s4d.client.guilds.create(String(${value_create})).then(async newServer => {
+  var guilder = `s4d.clients.channels.cache.get('${value_channel}').permissionOverwrites [
+  id: ${value_member},
+  ${value_action}: [
   ${statements}
-})`;
+ ]
+ },
+ ]`;
   return guilder;
 };
